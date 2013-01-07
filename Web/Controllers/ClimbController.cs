84e5d5cba6c50f102de84dc604353dfe295f2367
Padraig Farrell <pfarrel@tcd.ie>
2013-01-07 17:39:36 +0000

    Add scaffolded controllers/views for Climb/Location/Route

---
+++ Web/Controllers/ClimbController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class ClimbController : Controller
+    {
+        private ClimbrContext db = new ClimbrContext();
+
+        //
+        // GET: /Climb/
+
+        public ActionResult Index()
+        {
+            return View(db.Climbs.ToList());
+        }
+
+        //
+        // GET: /Climb/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Climb climb = db.Climbs.Find(id);
+            if (climb == null)
+            {
+                return HttpNotFound();
+            }
+            return View(climb);
+        }
+
+        //
+        // GET: /Climb/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Climb/Create
+
+        [HttpPost]
+        public ActionResult Create(Climb climb)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Climbs.Add(climb);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(climb);
+        }
+
+        //
+        // GET: /Climb/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Climb climb = db.Climbs.Find(id);
+            if (climb == null)
+            {
+                return HttpNotFound();
+            }
+            return View(climb);
+        }
+
+        //
+        // POST: /Climb/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Climb climb)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(climb).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(climb);
+        }
+
+        //
+        // GET: /Climb/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Climb climb = db.Climbs.Find(id);
+            if (climb == null)
+            {
+                return HttpNotFound();
+            }
+            return View(climb);
+        }
+
+        //
+        // POST: /Climb/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Climb climb = db.Climbs.Find(id);
+            db.Climbs.Remove(climb);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Web/Controllers/RouteController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class RouteController : Controller
+    {
+        private ClimbrContext db = new ClimbrContext();
+
+        //
+        // GET: /Route/
+
+        public ActionResult Index()
+        {
+            return View(db.Routes.ToList());
+        }
+
+        //
+        // GET: /Route/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Route route = db.Routes.Find(id);
+            if (route == null)
+            {
+                return HttpNotFound();
+            }
+            return View(route);
+        }
+
+        //
+        // GET: /Route/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Route/Create
+
+        [HttpPost]
+        public ActionResult Create(Route route)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Routes.Add(route);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(route);
+        }
+
+        //
+        // GET: /Route/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Route route = db.Routes.Find(id);
+            if (route == null)
+            {
+                return HttpNotFound();
+            }
+            return View(route);
+        }
+
+        //
+        // POST: /Route/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Route route)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(route).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(route);
+        }
+
+        //
+        // GET: /Route/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Route route = db.Routes.Find(id);
+            if (route == null)
+            {
+                return HttpNotFound();
+            }
+            return View(route);
+        }
+
+        //
+        // POST: /Route/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Route route = db.Routes.Find(id);
+            db.Routes.Remove(route);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}

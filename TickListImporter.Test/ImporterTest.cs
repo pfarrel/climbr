c55b87f30fbd78ac550720b60465c97f791cb0ae
Padraig Farrell <pfarrel@tcd.ie>
2013-01-18 14:22:21 +0000

    Create structure for Importer and ImporterTest projects

---
+++ TickListImporter.Test/ImporterTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TickListImporter.Test
+{
+    [TestClass]
+    public class ImporterTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

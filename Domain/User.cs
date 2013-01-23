2d3f3ba21f1b63bf6bb0c01848bac22addaf4ba0
Padraig Farrell <pfarrel@tcd.ie>
2013-01-23 22:20:58 +0000

    Separate Domain.User from Login/Account models

---
+++ Domain/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Domain
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key, DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public virtual List<Climb> Climbs { get; set; }
+        public virtual List<Route> AddedRoutes { get; set; }
+        public virtual List<Location> AddedLocations { get; set; }
+    }
+
+}
